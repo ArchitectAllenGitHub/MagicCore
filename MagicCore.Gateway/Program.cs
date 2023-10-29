99613b9977bb086ddae95b0ffc8538aaba746893
ArchitectAllen <1756196683@qq.com>
2023-10-29 14:20:09 +0800

    添加Ocelot.Provider.Polly包,后续有需求添加配置即可

--- MagicCore.Gateway/Program.cs
+++ MagicCore.Gateway/Program.cs
@@ -1,12 +1,14 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
+using Ocelot.Provider.Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddOcelot()
-    .AddConsul();
-    // .AddConfigStoredInConsul(); 同样的配置,存储在Consul 访问网关404 https://github.com/ThreeMammals/Ocelot/issues/1761
+    .AddConsul()
+    .AddPolly();// 限流/熔断
+// .AddConfigStoredInConsul(); 同样的配置,存储在Consul 访问网关404 https://github.com/ThreeMammals/Ocelot/issues/1761
 
 var app = builder.Build();
 
